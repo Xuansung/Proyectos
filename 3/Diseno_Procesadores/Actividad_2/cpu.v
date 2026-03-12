d35bb85b0da8bb2ec87654edb7f7cecf2e21c473
Alba Hidalgo <144910527+alsiahm@users.noreply.github.com>
2026-03-12 18:12:58 +0000

    Refactor cpu module to include r_inm wire

--- 3/Diseno_Procesadores/Actividad_2/cpu.v
+++ 3/Diseno_Procesadores/Actividad_2/cpu.v
@@ -2,11 +2,11 @@ module cpu(input wire clk, reset, output wire [9:0] pc_salida);
 //Procesador sin memoria de datos de un solo ciclo
 
     wire [7:0] opcode;
-    wire z, c, n, s_inc, we3, wf, m_wr, s_inm, push, pop;
+    wire z, c, n, s_inc, we3, wf, m_wr, r_inm, push, pop;
     wire [1:0] wd;
     wire [3:0] op_alu;
 
-    cd caminodatos(clk, reset, s_inc, s_inm, we3, wez, op_alu, z, opcode, pc_salida);
-    uc unidadcontrol(opcode, z, c, n, s_inc, we3, wf, m_wr, s_inm, push, pop, wd, op_alu);
+    cd caminodatos(clk, reset, r_inm, s_inc, r_inm, we3, wef, push, pop, m_wr, wd, op_alu, z, opcode, pc_salida);
+    uc unidadcontrol(opcode, z, c, n, s_inc, we3, wf, m_wr, r_inm, push, pop, wd, op_alu);
 
 endmodule
