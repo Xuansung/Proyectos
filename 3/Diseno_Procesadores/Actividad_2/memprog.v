4b45876d21ad64c1ba96894b43cf9df1659d144e
Alba Hidalgo <144910527+alsiahm@users.noreply.github.com>
2026-03-12 18:14:00 +0000

    Refactor memory modules for wider data handling

--- 3/Diseno_Procesadores/Actividad_2/memprog.v
+++ 3/Diseno_Procesadores/Actividad_2/memprog.v
@@ -2,9 +2,9 @@
 
 module memprog(input  wire        clk,
                input  wire [9:0]  a,
-               output wire [15:0] rd);
+               output wire [31:0] rd);
 
-  reg [15:0] mem[0:1023]; //memoria de 1024 palabras de 16 bits de ancho
+  reg [31:0] mem[0:1023]; //memoria de 1024 palabras de 16 bits de ancho
 
   initial
   begin
@@ -15,39 +15,31 @@ module memprog(input  wire        clk,
 
 endmodule
 
-module memdat(input wire clk, input wire [15:0] a, input wire [15:0] wd, input wire we3, output wire [15:0] s); // Modulo para la memoria de datos
-  reg [15:0] mem[65535:0];
+module memdat(input wire clk, input wire m_wr, input wire [15:0] a, input wire [15:0] wd, output wire [15:0] rd); // Modulo para la memoria de datos
+  reg [15:0] mem[0:1023];
 
-  mem[a] <= wd;
-  initial
-  begin
-    if (~we3) $readmemb("memdat.dat", mem);
-    else $writememb("memdat.dat", mem, a);
+
+  always @(posedge clk) begin
+    if (m_wr) mem[a] <= wd;
   end
 
   assign rd = mem[a];
 endmodule
 
-module stack(input wire clk, input wire [15:0] wd, input wire push, input wire pop, output wire [9:0] rd); // pila para los saltos de subrutina
-  reg [9:0] stackmem[0:1023];
-  reg [9:0] sp;
-
-
+module stack(input wire clk, input wire [9:0] wd, input wire push, input wire pop, output wire [9:0] rd, reset); // pila para los saltos de subrutina
+  reg [9:0] stackmem[0:15];
+  reg [3:0] sp;
   always @(posedge clk) begin
-    if (push) begin
+    if (reset) sp <= 4'b0;
+    else if (push) begin
+      stackmem[sp] <= wd;
       sp <= sp + 1;
-      stackmem[sp] <= a;
     end
     if (pop) begin
-      rd <= stackmem[sp];
       sp = sp - 1;
     end
-  end
 
-    initial
-  begin
-    if (push) $writememb("stack.dat", stackmem);
-    else if (pop) $readmemb("stack.dat", stackmem);
+    assign rd = stackmem[sp];
   end
 endmodule
 
