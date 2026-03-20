15281b484a9d3f2c9113f63cea205a299597e95e
Alba Hidalgo <144910527+alsiahm@users.noreply.github.com>
2026-03-20 18:45:30 +0000

    Refactor assembly instructions for clarity and correctness

--- 3/Diseno_Procesadores/Actividad_2_1/prueba.asm
+++ 3/Diseno_Procesadores/Actividad_2_1/prueba.asm
@@ -3,12 +3,12 @@ inicio:
     li 10 , r2
     add r3 , r1 , r2
     sub r4 , r2 , r1
-    eq r5 , r1 , r2
-    not r6 , r5 , r0
+    eq r5 , r1
+    not r6 , r5
     and r7 , r1 , r2
     or r8 , r1 , r2
     nega r9 , r1 , r0
-    negb r10 , r2 , r0
+    negb r10 , r0 , r2
     nop
     call subrutina
     addi r3 , r3 , 1
@@ -18,9 +18,9 @@ inicio:
 part2:
     subi r4 , r4 , 20
     jnn part3
-    sw r3 , r0 , 0
+    sw r3 , r1 , 2
     add r3 , r3 , r4
-    lw r5 , r0 , 0
+    lw r5 , r1 , 2
     subi r5 , r5 , 20
     jn part3
     addi r5 , r5 , 1
