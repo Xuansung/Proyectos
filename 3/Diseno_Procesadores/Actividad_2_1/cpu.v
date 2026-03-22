84d9a06d612646bf76e52a93f68bb27f7e56e8e5
Xuan <alu0101675411@ull.edu.es>
2026-03-22 15:05:41 +0000

    Rename wire 'z' to 'zero' in cpu.v

--- 3/Diseno_Procesadores/Actividad_2_1/cpu.v
+++ 3/Diseno_Procesadores/Actividad_2_1/cpu.v
@@ -1,8 +1,7 @@
 module cpu(input wire clk, reset, output wire [9:0] pc_salida);
     wire [7:0] opcode;
-    wire z, c, n, s_inc, wd, we3, wf, m_wr, r_inm, push, pop, halt;
+    wire zero, c, n, s_inc, wd, we3, wf, m_wr, r_inm, push, pop, halt;
     wire [3:0] op_alu;
-    cd caminodatos(clk, reset, r_inm, s_inc, we3, wf, push, pop, halt, m_wr, wd, op_alu, z, c, n, opcode, pc_salida);
-    
-    uc unidadcontrol(opcode, z, c, n, s_inc, wd, we3, wf, m_wr, r_inm, push, pop, halt, op_alu);
+    cd caminodatos(clk, reset, r_inm, s_inc, we3, wf, push, pop, halt, m_wr, wd, op_alu, zero, c, n, opcode, pc_salida);
+    uc unidadcontrol(opcode, zero, c, n, s_inc, wd, we3, wf, m_wr, r_inm, push, pop, halt, op_alu);
 endmodule
