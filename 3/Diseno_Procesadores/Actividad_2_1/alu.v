0c1b2b6b65299ecb5fa74c773ec0fa5e6b964030
Xuan <alu0101675411@ull.edu.es>
2026-03-22 01:19:02 +0000

    Change ALU operations to use variable 'b' instead of 'a'

--- 3/Diseno_Procesadores/Actividad_2_1/alu.v
+++ 3/Diseno_Procesadores/Actividad_2_1/alu.v
@@ -8,15 +8,14 @@ reg [16:0] s;
 always @(a, b, op_alu)
 begin
   case (op_alu) // extendemos el signo a 0 para operaciones lógicas y a 16 bits para operaciones aritméticas, de esta forma el bit 17 de s se usará como carry en suma y resta
-    4'b0000: s = {1'b0, a};              // EQ
-    4'b0001: s = {1'b0, ~a};             // NOT
+	4'b0000: s = {1'b0, b};              // EQ
+	4'b0001: s = {1'b0, ~b};             // NOT
     4'b0010: s = a + b;                  // ADD (aquí el carry sale solo)
     4'b0011: s = a - b;                  // SUB
     4'b0100: s = {1'b0, a & b};          // AND
     4'b0101: s = {1'b0, a | b};          // OR
     4'b0110: s = {1'b0, -a[15:0]};       // NEG A (Forzamos 16 bits antes de expandir)
     4'b0111: s = {1'b0, -b[15:0]};       // NEG B
-	4'b1000: s = {1'b0, b};            // B
     default: s = 17'b0;
   endcase
 end
