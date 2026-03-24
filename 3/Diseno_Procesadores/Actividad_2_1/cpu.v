878bfa618c13137556231ba3adee03f8e835327e
Alba Hidalgo <144910527+alsiahm@users.noreply.github.com>
2026-03-24 21:37:06 +0000

    Add timer functionality to CPU module

--- 3/Diseno_Procesadores/Actividad_2_1/cpu.v
+++ 3/Diseno_Procesadores/Actividad_2_1/cpu.v
@@ -1,7 +1,21 @@
-module cpu(input wire clk, reset, output wire [9:0] pc_salida);
+module cpu(input wire clk, reset, timer, output wire [9:0] pc_salida);
     wire [7:0] opcode;
     wire zero, c, n, s_inc, wd, we3, wf, m_wr, r_inm, push, pop, halt;
     wire [3:0] op_alu;
-    cd caminodatos(clk, reset, r_inm, s_inc, we3, wf, push, pop, halt, m_wr, wd, op_alu, zero, c, n, opcode, pc_salida);
-    uc unidadcontrol(opcode, zero, c, n, s_inc, wd, we3, wf, m_wr, r_inm, push, pop, halt, op_alu);
+    wire tick;
+    reg interrupcion;
+
+    timer_1s tempo(clk, reset, tick);
+
+    always @(posedge clk or posedge reset) begin
+        if (reset)
+            interrupcion <= 1'b0;
+        else if (tick)
+            interrupcion <= 1'b1;
+        else
+            interrupcion <= 1'b0;
+    end
+
+    cd caminodatos(interrupcion, clk, reset, r_inm, s_inc, we3, wf, push, pop, halt, m_wr, wd, ret, call, op_alu, zero, c, n, opcode, pc_salida);
+    uc unidadcontrol(opcode, zero, c, n, s_inc, wd, we3, wf, m_wr, r_inm, push, pop, halt, ret, call, op_alu);
 endmodule
