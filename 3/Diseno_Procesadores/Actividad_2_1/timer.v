4335071e5f69df3dfea45d9aa91c38379d7dfc7f
Alba Hidalgo <144910527+alsiahm@users.noreply.github.com>
2026-03-24 21:38:12 +0000

    Add timer module for 1 second tick generation

---
+++ 3/Diseno_Procesadores/Actividad_2_1/timer.v
@@ -0,0 +1,21 @@
+module timer_1s (
+   input wire clk, // 50 MHz clock input
+   input wire reset, // Active-high reset
+   output reg tick_1s // High for one clock cycle every second
+);
+   // 50 million cycles for 1 second at 50 MHz
+   parameter MAX_COUNT = 26'd4; // para simulacion 5 ciclos para generar un tick cada 3 instrucciones, en hardware real seria 26'd50_000_000
+   reg [25:0] counter;
+   always @(posedge clk or posedge reset) begin
+       if (reset) begin
+           counter <= 26'd0;
+           tick_1s <= 1'b0;
+       end else if (counter == MAX_COUNT) begin
+           counter <= 26'd0;
+           tick_1s <= 1'b1; // Generate tick
+       end else begin
+           counter <= counter + 1'b1;
+           tick_1s <= 1'b0;
+       end
+   end
+endmodule
