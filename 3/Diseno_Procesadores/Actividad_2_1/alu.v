eacd1ec104d49ab44c4fd7589a5059287449671e
Xuan <alu0101675411@ull.edu.es>
2026-04-10 23:43:10 +0100

    Refactor ALU module for improved arithmetic operations

--- 3/Diseno_Procesadores/Actividad_2_1/alu.v
+++ 3/Diseno_Procesadores/Actividad_2_1/alu.v
@@ -10,11 +10,12 @@ begin
   case (op_alu) // extendemos el signo a 0 para operaciones lógicas y a 16 bits para operaciones aritméticas, de esta forma el bit 17 de s se usará como carry en suma y resta
 	4'b0000: s = {1'b0, b};              // EQ
 	4'b0001: s = {1'b0, ~b};             // NOT
-    4'b0010: s = a + b;                  // ADD (aquí el carry sale solo)
-    4'b0011: s = a - b;                  // SUB
-    4'b0100: s = {1'b0, a & b};          // AND
-    4'b0101: s = {1'b0, a | b};          // OR
-    4'b0110: s = -{b[15], b};         // NEG (aquí el carry sale solo)
+    // Concatenamos un 0 al inicio para forzar aritmética de 17 bits sin signo
+  4'b0010: s = {1'b0, a} + {1'b0, b};  // ADD
+  4'b0011: s = {1'b0, a} - {1'b0, b};  // SUB (s[16] será 1 si b > a)
+  4'b0100: s = {1'b0, a & b};          // AND
+  4'b0101: s = {1'b0, a | b};          // OR
+  4'b0110: s = 17'd0 - {1'b0, b};         // NEG
     default: s = 17'b0;
   endcase
 end
@@ -22,7 +23,7 @@ end
 assign y = s[15:0];
 
 //Calculo del flag de cero
-assign zero = ~(|y[15:0]);   //operador de reducción |y hace la or de los bits del vector 'y' y devuelve 1 bit resultado
+assign zero = (y == 16'b0); 
 assign zneg = y[15]; //El bit más significativo si es negativo.
 assign zcarry = s[16]; //El bit 17 de carry. 
 endmodule
